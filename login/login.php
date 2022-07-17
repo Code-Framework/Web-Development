33e2443aec2f4b54f63cbce2a810a8d88a8f4337
“aman” <amandeep35031@gmail.com>
2022-07-17 18:53:55 +0530

    ok

--- login/login.php
+++ login/login.php
@@ -8,11 +8,11 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 if(isset($_POST['submit']))
-{     $log_pass = $_POST['log_pass'];
-     $log_user = $_POST['log_user'];
+{    $user = $_POST['log_user'];
+     $pass = $_POST['logpass'];
    
      $sql = "INSERT INTO conn (log_user,log_pass)
-     VALUES ('$log_user','$log_pass')";
+     VALUES ('$user','$pass')";
      if (mysqli_query($link, $sql)) {
         echo "New record has been added successfully !";
      } else {
