fc4c4e97c43d7914c8836a9acb94a4fc428a50b1
“aman” <amandeep35031@gmail.com>
2022-07-17 18:34:57 +0530

    hlo

--- login/login.php
+++ login/login.php
@@ -1,23 +1,23 @@
 <?php
-echo '<pconnected to databse"</p>'
-    session_start();
-    include('config.php');
-    if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $query = $connection->prepare("SELECT * FROM users WHERE username=:username");
-        $query->bindParam("username", $username, PDO::PARAM_STR);
-        $query->execute();
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-        if (!$result) {
-            echo '<p class="error">Username password combination is wrong!</p>';
-        } else {
-            if (password_verify($password, $result['password'])) {
-                $_SESSION['user_id'] = $result['id'];
-                echo '<p class="success">Congratulations, you are logged in!</p>';
-            } else {
-                echo '<p class="error">Username password combination is wrong!</p>';
-            }
-        }
-    }
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "id19251342_check", "Aa@123456789", "id19251342_conn");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+if(isset($_POST['submit']))
+{     $log_pass = $_POST['log_pass'];
+     $log_user = $_POST['log_user'];
+   
+     $sql = "INSERT INTO conn (log_user,log_pass)
+     VALUES ('$log_user','$log_pass')";
+     if (mysqli_query($link, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($link);
+}
 ?>
